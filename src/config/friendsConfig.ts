bb658c860e14bf0085145d2e6dc2f6d34fe89dfb
0n1y <bohaozhou349@gmail.com>
2026-04-22 19:36:47 +0800

    11111

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -58,6 +58,15 @@ export const friendsConfig: FriendLink[] = [
 		// tags: ["Docs"],
 		weight: 7,
 		enabled: true,
+    },
+	{
+        title: "Flakes",
+        imgurl: "https://www.0n1y.org/color.jpg",
+        desc: "彩天坊!?",
+        siteurl: "https://flakes.ink",
+		// tags: ["Docs"],
+		weight: 6,
+		enabled: true,
     }
 ];
 
